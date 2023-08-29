df99a32a9744da0d3efa8994cb9af57046e060ca
virajvoditel <98550775+virajvoditel@users.noreply.github.com>
2023-08-29 15:55:56 +0530

    Update [[...catchall]].tsx

--- pages/[[...catchall]].tsx
+++ pages/[[...catchall]].tsx
@@ -49,7 +49,21 @@ export default function PlasmicLoaderPage(props: {
           gtag('config', 'G-V2YWMDGN8F');
         `}
       </Script>
-
+      <Script id="linkedin-analytics" strategy="afterInteractive">
+        {`
+          _linkedin_partner_id = "5815073";
+          window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+          window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+          (function(l) {
+          if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+          window.lintrk.q=[]}
+          var s = document.getElementsByTagName("script")[0];
+          var b = document.createElement("script");
+          b.type = "text/javascript";b.async = true;
+          b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+          s.parentNode.insertBefore(b, s);})(window.lintrk);
+        `}
+      </Script>
       <PlasmicComponent component={plasmicData.entryCompMetas[0].name} />
     </PlasmicRootProvider>
   );
